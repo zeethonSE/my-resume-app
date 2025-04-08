e400a1e2b4566c068968cdb0a1be6d2c06a24427
zeethonSE <zeethon0@gmail.com>
2025-04-08 20:46:22 +0700

    change base position to top

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
   base: '/my-resume-app/',
+  plugins: [react(), tailwindcss(),] 
 });
