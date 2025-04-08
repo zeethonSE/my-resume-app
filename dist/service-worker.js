23d93f612a83cdf96848df3d9c449081fa6afe6c
zeethonSE <zeethon0@gmail.com>
2025-04-08 21:58:39 +0700

    Removed dist from tracking

--- dist/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (event) => {
-    console.log('Service Worker installed');
-});
-
-self.addEventListener('fetch', (event) => {
-    console.log('Fetching:', event.request.url);
-});
