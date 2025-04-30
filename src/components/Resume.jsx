33367df9f5cd82f56f45afab31f054f73abfe520
zeethonSE <zeethon0@gmail.com>
2025-04-30 17:35:46 +0700

    Update Resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -36,6 +36,30 @@ function Resume() {
                             <p key={i}>{lang.name} - {lang.level}</p>
                         ))}
                     </div>
+                    <a
+                        href="./assets/SSai-WD-Resume-2025.pdf"
+                        download
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="btn btn-primary"
+                        >
+                        <h5 className="text-transparent bg-clip-text bg-gradient-to-r from-green-800 via-green-200 to-blue-800 text-sm font-bold flex items-center gap-2">
+                            Download Resume
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="20"
+                                height="20"
+                                fill="#1f2930"
+                                className="bi bi-arrow-down-circle animate-bounce"
+                                viewBox="0 0 16 16"
+                            >
+                                <path
+                                fillRule="evenodd"
+                                d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"
+                                />
+                            </svg>
+                        </h5>
+                    </a>
                 </div>
                 ))}
             
